88cd8471546352c8a9017edc0c95aed7e230c40f
oussamatght <oussamatght6@gmail.com>
2025-08-31 03:42:40 +0200

    Initial commit with new version

--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337), // this ensures Render can assign the port
+    port: env.int('PORT', 1337),
     app: {
         keys: env.array('APP_KEYS', [
             '6920dc8a103869af4d14fbacd1119ede919b61c4a6015785c5e49c858780471b7b9be09de87077ecc28724622001f8f16cdb117664fdce6d0f0ee9d0d52ff22748156e6fb2046b0fef9e92922452bfd580a796b9ecf30b44e92f4e0dd03f337530c1e346d8835c15c9839afa8235627a5b6c3ea6f46db9f70ca511d0353af47e',
         ]),
+    },
+    webhooks: {
+        populateRelations: env.bool('WEBHOOKS_POPULATIONS_RELATIONS', false),
     },
 });
