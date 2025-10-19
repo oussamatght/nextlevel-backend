4b74f7f626727cc741995c3226a21d030db8ab0b
oussama <oussamatght6@gmail.com>
2025-10-19 18:32:02 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,62 +1,66 @@
 export default [
-    'strapi::logger',
-    'strapi::errors',
-    {
-        name: 'strapi::security',
-        config: {
-            contentSecurityPolicy: {
-                useDefaults: true,
-                directives: {
-                    'connect-src': ["'self'", 'https:'],
-                    'img-src': [
-                        "'self'",
-                        'data:',
-                        'blob:',
-                        'https://market-assets.strapi.io',
-                        'https://console.cloudinary.com',
-                        'https://res.cloudinary.com',
-                    ],
-                    'script-src': [
-                        "'self'",
-                        'example.com',
-                        'https://media-library.cloudinary.com',
-                        'https://upload-widget.cloudinary.com',
-                        'https://console.cloudinary.com',
-                    ],
-                    'media-src': [
-                        "'self'",
-                        'data:',
-                        'blob:',
-                        'https://console.cloudinary.com',
-                        'https://res.cloudinary.com',
-                    ],
-                    'frame-src': [
-                        "'self'",
-                        'https://media-library.cloudinary.com',
-                        'https://upload-widget.cloudinary.com',
-                        'https://console.cloudinary.com',
-                    ],
-                    upgradeInsecureRequests: null,
-                },
-            },
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://console.cloudinary.com',
+            'https://res.cloudinary.com',
+          ],
+          'script-src': [
+            "'self'",
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://console.cloudinary.com',
+            'https://res.cloudinary.com',
+          ],
+          'frame-src': [
+            "'self'",
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
         },
+      },
     },
-    {
-        name: 'strapi::cors',
-        config: {
-                       origin: [
-  'https://nextlevel-m5ho.onrender.com',
-  'http://localhost:3000',
-  'https://nextlevel-brown.vercel.app',
-   'https://nextlevel-2.onrender.com',                        
-],
+  },
 
-        },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000',
+        'https://nextlevel-m5ho.onrender.com',
+        'https://nextlevel-brown.vercel.app',
+        'https://nextlevel-2.onrender.com',
+      ],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      keepHeaderOnError: true,
     },
-    'strapi::poweredBy',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
+  },
+
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
